f1d186c811f7a879b13dd3368f1a839ac5515c95
Ferdian <ferdianrafli25@gmail.com>
2022-11-14 17:11:36 +0700

    fix: date kegiatan

--- resources/views/user/detail-kegiatan.blade.php
+++ resources/views/user/detail-kegiatan.blade.php
@@ -1,32 +1,33 @@
 @extends('layouts/template-user')
 
 @section('content')
-    <div class="wx mx-auto pt60" id="detailKegiatan">
-        <div class="row">
-            <div class="col-lg-7 col-12">
-                <h3 class="fw-bold">Detail Kegiatan</h3>
-                <div class="image mt-2">
-                    <img src="{{ asset('storage/' . $news->gallery[0]->photo) }}" alt="Foto Detail Kegiatan">
-                </div>
-                <div class="content mt-2">
-                    <h4 class="blue fwbold">{{ $news->title }}</h4>
-                    <p class="darkgrey mt-1 mb-3">{{ $news->updated_at }}</p>
-                    {!! $news->content !!}
-                </div>
+<div class="wx mx-auto pt60" id="detailKegiatan">
+    <div class="row">
+        <div class="col-lg-7 col-12">
+            <h3 class="fw-bold">Detail Kegiatan</h3>
+            <div class="image mt-2">
+                <img src="{{ asset('storage/' . $news->gallery[0]->photo) }}" alt="Foto Detail Kegiatan">
             </div>
-            <div class="col-lg-4 col-12 offset-lg-1 mt-lg-0 mt-3">
-                <h5 class="fw-bold mb-2">Kegiatan Lainnya</h5>
-                <div class="card__lainnya row flex-lg-column flex-row">
-                    @foreach ($other_news as $other_new)
-                        <div class="col-lg-12 col-sm-6 col-md-4">
-                            <div class="image mb-2">
-                                <img src="/img/maintenance.jpg" alt="Foto Detail Kegiatan">
-                            </div>
-                            <a href="/news/{{ $other_new->id }}" class="blue text-decoration-none fwbold">{{ $other_new->title }}</a>
-                            <p class="darkgrey">{{ $other_new->updated_at }}</p>
-                        </div>
-                    @endforeach
-                    {{-- <div class="col-lg-12 col-sm-6 col-md-4">
+            <div class="content mt-2">
+                <h4 class="blue fwbold">{{ $news->title }}</h4>
+                <p class="darkgrey mt-1 mb-3">{{ $news->updated_at }}</p>
+                {!! $news->content !!}
+            </div>
+        </div>
+        <div class="col-lg-4 col-12 offset-lg-1 mt-lg-0 mt-3">
+            <h5 class="fw-bold mb-2">Kegiatan Lainnya</h5>
+            <div class="card__lainnya row flex-lg-column flex-row">
+                @foreach ($other_news as $other_new)
+                <div class="col-lg-12 col-sm-6 col-md-4">
+                    <div class="image mb-2">
+                        <img src="/img/maintenance.jpg" alt="Foto Detail Kegiatan">
+                    </div>
+                    <a href="/news/{{ $other_new->id }}"
+                        class="blue text-decoration-none fwbold">{{ $other_new->title }}</a>
+                    <p class="darkgrey">{{ date_format($other_new->updated_at,"d/m/Y H:i") }}</p>
+                </div>
+                @endforeach
+                {{-- <div class="col-lg-12 col-sm-6 col-md-4">
                         <div class="image mb-2">
                             <img src="/img/maintenance.jpg" alt="Foto Detail Kegiatan">
                         </div>
@@ -42,8 +43,8 @@
                             Adha 1443H</a>
                         <p class="darkgrey">10 Agustus 2022</p>
                     </div> --}}
-                </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
--- resources/views/user/kegiatan.blade.php
+++ resources/views/user/kegiatan.blade.php
@@ -1,34 +1,34 @@
 @extends('layouts/template-user')
 
 @section('content')
-    <div class="kegiatan">
-        <div class="pt60 mx-auto">
-            <div class="position-relative header">
-                <h2 class="mx-auto darkblue fw-bold">Kegiatan</h2>
-                <span class="position-absolute"></span>
-            </div>
-            <h5 class="text-center mt-4">Beberapa kegiatan yang telah kami lakukan</h5>
+<div class="kegiatan">
+    <div class="pt60 mx-auto">
+        <div class="position-relative header">
+            <h2 class="mx-auto darkblue fw-bold">Kegiatan</h2>
+            <span class="position-absolute"></span>
         </div>
-        <div class="mt-3 wx mx-auto">
-            <div class="row">
-                @foreach ( $news as $new)
-                <div class="col-xl-4 col-md-6 col-12">
-                    <div class="card bgwhite p-4">
-                        <div class="card__image">
-                            <img src="{{ asset('storage/'.$new->gallery[0]->photo) }}" alt="Foto Kegiatan">
-                        </div>
-                        <div class="mt-2 card__header">
-                            <h5 class="fwbold blue">{{ $new->title }}</h5>
-                            <p class="darkgrey mt-1 mb-0">{{ $new->updated_at }}</p>
-                        </div>
-                        <div class="mt-3 card__desc">
-                            <h6 class="mb-2">{!! $new->excerpt !!}</h6>
-                            <a href="/news/{{ $new->id }}" class="text-decoration-none">Baca Selengkapnya</a>
-                        </div>
+        <h5 class="text-center mt-4">Beberapa kegiatan yang telah kami lakukan</h5>
+    </div>
+    <div class="mt-3 wx mx-auto">
+        <div class="row">
+            @foreach ( $news as $new)
+            <div class="col-xl-4 col-md-6 col-12">
+                <div class="card bgwhite p-4">
+                    <div class="card__image">
+                        <img src="{{ asset('storage/'.$new->gallery[0]->photo) }}" alt="Foto Kegiatan">
+                    </div>
+                    <div class="mt-2 card__header">
+                        <h5 class="fwbold blue">{{ $new->title }}</h5>
+                        <p class="darkgrey mt-1 mb-0">{{ date_format($new->updated_at,"d/m/Y H:i") }}</p>
+                    </div>
+                    <div class="mt-3 card__desc">
+                        <h6 class="mb-2">{!! $new->excerpt !!}</h6>
+                        <a href="/news/{{ $new->id }}" class="text-decoration-none">Baca Selengkapnya</a>
                     </div>
                 </div>
-                @endforeach
-                <!-- <div class="col-xl-4 col-md-6 col-12">
+            </div>
+            @endforeach
+            <!-- <div class="col-xl-4 col-md-6 col-12">
                     <div class="card bgwhite p-4">
                         <div class="card__image">
                             <img src="/img/repairing.jpg" alt="Foto Kegiatan">
@@ -79,7 +79,7 @@
                         </div>
                     </div>
                 </div> -->
-            </div>
         </div>
     </div>
+</div>
 @endsection
